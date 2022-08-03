f4d1f17925d1beefbd30789f1feb20232b437c97
Pedro Sousa <petrelli100@gmail.com>
2022-08-03 15:35:06 +0100

    End Of Section 5

---
+++ NZWalks/NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,129 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("walks")]
+    public class WalksController : Controller
+    {
+        private readonly IWalkRepository walkRepository;
+        private readonly IMapper mapper;
+
+        public WalksController(IWalkRepository walkRepository, IMapper mapper)
+        {
+            this.walkRepository = walkRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalksAsync()
+        {
+            var walks = await walkRepository.GetAllAsync();
+
+            var walksDTO = mapper.Map<List<Models.DTO.Walk>>(walks);
+
+            return Ok(walksDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetWalkAsync")]
+        public async Task<IActionResult> GetWalkAsync(Guid id)
+        {
+            var walk = await walkRepository.GetAsync(id);
+
+            if (walk == null)
+            {
+                return NotFound();
+            }
+
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+
+            return Ok(walkDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkAsync(AddWalkRequest addWalkRequest)
+        {
+            // Request(DTO) to Domain model
+            //var walk = mapper.Map<Models.Domain.Walk>(addWalkRequest);
+            var walk = new Models.Domain.Walk()
+            {
+                Name = addWalkRequest.Name,
+                Length = addWalkRequest.Length,
+                RegionId = addWalkRequest.RegionId,
+                WalkDifficultyId = addWalkRequest.WalkDifficultyId
+            };
+
+            // Pass details to Repository
+            walk = await walkRepository.AddAsync(walk);
+
+            // Convert back to DTO
+            //var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+            var walkDTO = new Models.Domain.Walk()
+            {
+                Id = walk.Id,
+                Name = walk.Name,
+                Length = walk.Length,
+                RegionId = walk.RegionId,
+                WalkDifficultyId = walk.WalkDifficultyId
+            };
+
+            return CreatedAtAction(nameof(GetWalkAsync), new { id = walkDTO.Id }, walkDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateWalkAsync([FromRoute]  Guid id, [FromBody] UpdateWalkRequest updateWalkRequest)
+        {
+            // Request(DTO) to Domain model
+            var walk = new Models.Domain.Walk()
+            {
+                Name = updateWalkRequest.Name,
+                Length = updateWalkRequest.Length,
+                RegionId = updateWalkRequest.RegionId,
+                WalkDifficultyId = updateWalkRequest.WalkDifficultyId
+            };
+
+            walk = await walkRepository.UpdateAsync(id, walk);
+
+            if (walk == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(walk);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteWalkAsync(Guid id)
+        {
+            // Get walk from database
+            var walk = await walkRepository.DeleteAsync(id);
+
+            // If null NotFound
+            if (walk == null)
+            {
+                return NotFound();
+            }
+
+            // Convert response back to DTO
+            //var walkDTO = new Models.DTO.Walk()
+            //{
+            //    Name = walk.Name,
+            //    Length = walk.Length,
+            //    RegionId = walk.RegionId,
+            //    WalkDifficultyId = walk.WalkDifficultyId
+            //};
+
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+
+            // Return Ok response
+            return Ok(walkDTO);
+        }
+    }
+}
